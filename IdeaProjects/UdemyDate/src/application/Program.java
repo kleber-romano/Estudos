74e7ad04bb8b094ec011754b3c70b5e26fce1eb8
kleber-romano <kleber.romano@gmail.com>
2021-06-19 22:17:02 -0300

    Atualização
    HTML - primeiros passos
    Java - date

--- IdeaProjects/UdemyDate/src/application/Program.java
+++ IdeaProjects/UdemyDate/src/application/Program.java
@@ -1,2 +1,24 @@
-package application;public class Program {
+package application;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class Program {
+    public static void main(String[] args) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+        Date d = Date.from(Instant.parse("2021-06-09T17:25:03Z"));
+        System.out.println(sdf.format(d));
+
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(d);
+        int minutes = cal.get(Calendar.HOUR);
+
+        System.out.println(sdf.format(d));
+        System.out.println("Minutes: " + minutes);
+    }
 }
