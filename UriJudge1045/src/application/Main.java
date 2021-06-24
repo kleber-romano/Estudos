6a1933c5064360debe9fb58daec86dbf677d2294
kleber-romano <kleber.romano@gmail.com>
2021-06-24 19:40:01 -0300

    Resolucao do exercicio 1045 da plataforma Uri Judge. Adicionada a lógica de terminação da ordem decrescente dos valores de entrada para efetuar os codigos

--- UriJudge1045/src/application/Main.java
+++ UriJudge1045/src/application/Main.java
@@ -5,28 +5,67 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double a, b, c;
+        double x, y, z, a, b, c;
 
-        a = sc.nextDouble();
-        b = sc.nextDouble();
-        c = sc.nextDouble();
+        x = sc.nextDouble();
+        y = sc.nextDouble();
+        z = sc.nextDouble();
+        a = 0;
+        b = 0;
+        c = 0;
 
-        if (a >= b+c){
+        if(x > y && x > z){
+            if(y > z) {
+                a = x;
+                b = y;
+                c = z;
+            } else{
+                a = x;
+                b = z;
+                c = y;
+            }
+        } else if(y > x && y > z){
+            if(x > z){
+                a = y;
+                b = x;
+                c = z;
+            }else{
+                a = y;
+                b = z;
+                c = x;
+            }
+        }else if (z > x && z > y){
+            if (x > y){
+                a = z;
+                b = x;
+                c = y;
+            }else{
+                a = z;
+                b = y;
+                c = x;
+            }
+        }else{
+            a = x;
+            b = y;
+            c = z;
+        }
+
+        if (a >= (b+c)) {
             System.out.println("NAO FORMA TRIANGULO");
+        }else {
+            if (a * a == b * b + c * c) {
+                System.out.println("TRIANGULO RETANGULO");
+            } else if (a * a > b * b + c * c) {
+                System.out.println("TRIANGULO OBTUSANGULO");
+            } else if (a * a < b * b + c * c) {
+                System.out.println("TRIANGULO ACUTANGULO");
+            }
+            if (a == b && b == c) {
+                System.out.println("TRIANGULO EQUILATERO");
+            } else if (a == b || b == c || c == a) {
+                System.out.println("TRIANGULO ISOSCELES");
+            }
         }
-        if (a*a == b*b+c*c){
-            System.out.println("TRIANGULO RETANGULO");
-        }
-        if (a*a > b*b+c*c){
-            System.out.println("TRIANGULO OBTUSANGULO");
-        }
-        if (a*a < b*b+c*c){
-            System.out.println("TRIANGULO ACUTANGULO");
-        }
-        if (a == b && b==c){
-            System.out.println("TRIANGULO EQUILATERO");
-        } else if (a == b || b==c || c==a ){
-            System.out.println("TRIANGULO ISOSCELES");
-        }
+
     }
 }
